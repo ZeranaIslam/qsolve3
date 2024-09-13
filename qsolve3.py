b4f01b34a664bd045b3d12937fb35b635db0a88d
ZeranaIslam <zeranaislam123@gmail.com>
2024-09-13 20:22:03 +0600

    Create qsolve3.py

---
+++ qsolve3.py
@@ -0,0 +1,11 @@
+3.Create a program that converts temperatures between Fahrenheit and Celsius based on user input.
+
+
+celsius = float(input("Enter temperature\
+in celsius: "))
+ 
+fahrenheit = (celsius * 1.8) + 32
+ 
+print(str(celsius )+ " degree Celsius\
+is equal to " + str(fahrenheit )+
+       " degree Fahrenheit.")
